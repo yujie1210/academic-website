79d0cc27322143a0eb24d1e4b78df7121ce3e929
yujiefan <yujiefan2023@163.com>
2026-08-23 19:37:28 +0800

    Use a chevron instead of a plus sign

--- build_pages.py
+++ build_pages.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 ROOT = Path(__file__).parent
 SITE = "https://yujie1210.github.io/academic-website"
-VERSION = "20260823t"
+VERSION = "20260823u"
 
 NAV = [
     ("index.html", "Home"),
